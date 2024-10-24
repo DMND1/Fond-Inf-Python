add887f4bb20a4113326608fcae58dfd6204171a
Francesco Cirmignani <fcirmignani@gmail.com>
2024-10-24 10:24:48 +0200

    Lezione 24/10/2024 prima parte

---
+++ Programmi slide/Variabili e tipi/Conversione tra tipi.py
@@ -0,0 +1,19 @@
+targa = "DE" + 295 + "WK" # Errore: 295 non è una stringa
+
+str(123) #converte til valore in stringa
+
+targa = "DE" + str(123) + "WK"
+
+print(targa)
+
+
+#si può anche fare con le variabili
+a = 321
+
+targa = "DE" + str(a) + "WK"
+
+print(targa)
+
+
+print(int("12.4")) #Da errore perché la stringa "12.4" non è un intero
+print(int(float("12.4"))) #stampa 12
