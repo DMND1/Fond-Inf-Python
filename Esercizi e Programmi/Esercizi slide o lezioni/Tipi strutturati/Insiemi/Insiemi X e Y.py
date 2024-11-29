e212a2c4a04d721ef04a88e60dbe08a8753172b5
Francesco Cirmignani <fcirmignani@gmail.com>
2024-11-29 11:27:34 +0100

    Lezione del 29/11/2024 Parte 3

--- Esercizi e Programmi/Esercizi slide o lezioni/Tipi strutturati/Insiemi/Insiemi X e Y.py
+++ Esercizi e Programmi/Esercizi slide o lezioni/Tipi strutturati/Insiemi/Insiemi X e Y.py
@@ -1,30 +1,48 @@
 # Scrivere un progamma che costruisca gli insiemi X dei quadrati minori di 1000 e l’insieme Y dei cubi minori di 1000 e verifichi se esiste un naturale il cui predecessore è un 
 # quadrato e il cui successore è un cubo.
-
+limite = 1000
 X = set()
 Y = set()
 
 i = 1
-while (i ** 2) < 1000:
+while (i ** 2) < limite:
     X.add(i ** 2)
     i += 1
 
 i = 1
-while (i ** 3) < 1000:
+while (i ** 3) < limite:
     Y.add(i ** 3)
     i += 1
 
+intersezione = set()
 
+for elememnto_y in Y:
+    for elemento_x in X:
+        if (elememnto_y - 1) == (elemento_x + 1):
+            intersezione.add(elememnto_y - 1)
+
+print(intersezione)
+
+
+
+# O in alternativa:
+for cubo in Y:
+    if (cubo - 2) in X:
+        intersezione.add(cubo - 1)
+
+print(intersezione)
+
+
+
+# O in alternativa:
 predecessori_quadrati = set()
 successori_cubi = set()
-
 
 for elem in X:
     predecessori_quadrati.add(elem + 1)
 
 for elem in Y:
     successori_cubi.add(elem - 1)
-
 
 intersezione = predecessori_quadrati.intersection(successori_cubi)
 print(intersezione)
@@ -35,17 +53,15 @@ print(intersezione)
 predecessori_quadrati = set()
 successori_cubi = set()
 
-
 i = 1
-while (i ** 2) < 1000:
+while (i ** 2) < limite:
     predecessori_quadrati.add(i ** 2 + 1)
     i += 1
 
 i = 1
-while (i ** 3) < 1000:
+while (i ** 3) < limite:
     successori_cubi.add(i ** 3 - 1)
     i += 1
-
 
 intersezione = predecessori_quadrati.intersection(successori_cubi)
 print(intersezione)
--- Esercizi e Programmi/Esercizi slide o lezioni/Tipi strutturati/Insiemi/La frase contiene tutte le lettere.py
+++ Esercizi e Programmi/Esercizi slide o lezioni/Tipi strutturati/Insiemi/La frase contiene tutte le lettere.py
@@ -1,14 +1,34 @@
-# Chiedere all’utente una frase e verificare se contiene tutte le lettere dell’alfabeto.
+# Creazione dell'insieme di tutte le letter dell'alfabeto
 
+alfabeto = set()
+c = "a"
+while ord(c) <= ord("z"):
+    alfabeto.add(c)
+    c = chr(ord(c) + 1)
+
+
+# Chiedere all’utente una frase e verificare se contiene tutte le lettere dell’alfabeto.
 frase = input("Inserire una frase: ")
+
+def tutteLettereAlfabeto(stringa):
+    stringa = stringa.lower()
+
+    alfabeto = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+    lettere = set(stringa)
+
+    if alfabeto.issubset(lettere):
+        return True
+    else:
+        return False
+
+
+# In alternativa:
+frase = input("Inserire una frase: ")
+print(tutteLettereAlfabeto(frase))
 
 frase = frase.lower()
 frase = frase.replace(" ", "")
-frase = frase.replace(".", "")
-frase = frase.replace(",", "")
-frase = frase.replace("!", "")
-frase = frase.replace(";", "")
-frase = frase.replace(":", "")
 
 alfabeto = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
 
